2f6a08520839fdc6e9fa5eadb771a33d224dcd15
Ignacio Cordoba <icordoba@4geeks.co>
2018-09-27 00:49:14 +0000

    Initial commit

---
+++ src/js/layout.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import {Home} from './views/home.jsx';
+import {Product} from './views/product.jsx';
+import {Blog} from './views/blog.jsx';
+import {Post} from './views/post.jsx';
+import {Checkout} from './views/checkout.jsx';
+import {Login} from './views/login.jsx';
+
+
+//create your first component
+export class Layout extends React.Component{
+    
+    render(){
+        return (
+            <React.Fragment>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/product" component={Product} />
+                        <Route path="/blog" component={Blog} />
+                        <Route path="/post" component={Post} />
+                        <Route path="/checkout" component={Checkout} />
+                        <Route path="/login" component={Login} />
+                        <Route render={() => <h1>Not found!</h1>} />
+                    </Switch>
+                </BrowserRouter>
+            </React.Fragment>
+        );
+    }
+}
---
+++ src/js/views/product.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Product extends React.Component{
+    render(){
+        return(<h1>HELLO PRODUCT</h1>);
+    }
+}
