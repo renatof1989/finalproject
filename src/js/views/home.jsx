f7f1c032aa6e6d85e6741d6303d008466d626a57
Olson Dimanche <yashiro@Olsons-MacBook-Pro.local>
2018-09-28 19:55:16 -0400

    not much

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -6,7 +6,6 @@ import {Card} from "../component/home/card.jsx";
 export class Home extends React.Component{
     render(){
         return(<div>
-            <h1> This home is now clear </h1>
             <NavBar />
 
             <Card image="..." header="Header" text="Eat My Shorts"/>
