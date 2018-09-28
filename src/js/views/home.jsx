ba8bc961770c8f416c07bef5b375b35fdfcaafbc
Javier Sarcar <libertypunk5@gmail.com>
2018-09-28 21:58:35 +0000

    first attempt

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -2,6 +2,43 @@ import React from "react";
 
 export class Home extends React.Component{
     render(){
-        return(<h1>HELLO HOME</h1>);
+        return(<div>
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                    <a class="navbar-brand" href="#">Carousel</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+        
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Disabled</a>
+                            </li>
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+            <div>
+                <footer class="fixed-bottom container border-top">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-8">© 2017-2018 Company, Inc. · Privacy · Terms</div>
+                            <div class="col-4">Back to Top</div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>);
     }
 }
