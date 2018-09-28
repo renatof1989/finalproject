770cc54ec15e7ddf43819ff97182c8cc68030262
Carlos Rivera <crivera2596@gmail.com>
2018-09-28 23:11:57 +0000

    Working on the card

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -7,7 +7,32 @@ export class Home extends React.Component{
             <h1> This home is now clear </h1>
             <NavBar />
 
-                
+            <Card image="..." header="Header" text="Eat My Shorts"/>
         </div>);
+    }
+}
+
+class Card extends React.Component {
+    
+    constructor(props) {
+        super();
+        this.state = {
+            image: props.img,
+            header: props.head,
+            body: props.text
+        };
+    }
+    
+    render() {
+        return (
+            <div className="card col-lg-4">
+                <img className="card-img-top" src={this.state.image} />
+                <div className="card-body">
+                    <h2>{this.state.header}</h2>
+                    <p>{this.state.body}</p>
+                </div>
+                <a className="btn btn-secondary" href="/blog" role="button">View Details</a>
+            </div>
+        );
     }
 }
