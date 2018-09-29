d4cbe927d473834ff401b763db604031102ed71a
FrankMan <fmanzanarez@outlook.com>
2018-09-29 00:53:21 +0000

    CarouselItem function made and error checking

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavBar from "./navbar.jsx";
 
 import {Carousel} from "../component/home/carousel.jsx";
-import {Card} from "../component/home/card.jsx";
+//import {Card} from "../component/home/card.jsx";
 
 export class Home extends React.Component{
     render(){
