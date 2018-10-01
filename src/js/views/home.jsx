fcac6f6d0a75705a546711a134ac53f14fd4066c
Carlos Rivera <crivera2596@gmail.com>
2018-10-01 22:48:27 +0000

    React hates our carousel

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -3,15 +3,18 @@ import NavBar from "./navbar.jsx";
 
 import {Carousel} from "../component/home/carousel.jsx";
 //import {Card} from "../component/home/card.jsx";
+import {Random} from "../component/home/random.jsx";
 
 export class Home extends React.Component{
     render(){
         return(
             <div>
-                <h1> This home is now clear </h1>
                 <NavBar />
-    
-                <Carousel />
+                
+                <main role="main">
+                    <Carousel />
+                    <Random />
+                </main>
             </div>
         );
     }
