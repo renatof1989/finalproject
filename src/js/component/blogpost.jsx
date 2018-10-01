d41c2a1212b38eb827609c75131158584b43c6f9
Renato Froes <renatofroes@gmail.com>
2018-10-01 22:55:39 +0000

    blog creation

---
+++ src/js/component/blogpost.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class BlogPost extends React.Component {
+    render(){
+      return (
+          <div>
+          <h1></h1>
+          </div>
+      );
+    }
+}
