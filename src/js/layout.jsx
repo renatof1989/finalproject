cc60328d2200fe1ace6dcccf8aad7b5122a40263
Olson Dimanche <yashiro@Olsons-MacBook-Pro.local>
2018-10-03 19:34:53 -0400

    updated home

--- src/js/layout.jsx
+++ src/js/layout.jsx
@@ -8,7 +8,7 @@ import {Post} from './views/post.jsx';
 import {Checkout} from './views/checkout.jsx';
 import {Login} from './views/login.jsx';
 import {Demo} from './views/demo.jsx';
-
+import {UserInfo} from './views/userInfo.jsx';
 import {Context} from './store/appContext.jsx';
 
 //create your first component
