440ec2e8bcba24a8d342c0da2cb3ef8d57b8a5f2
Javier Sarcar <libertypunk5@gmail.com>
2018-10-04 00:33:20 +0000

    update

--- src/js/views/navbar.jsx
+++ src/js/views/navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 class NavBar extends Component{
     render(){
         return(
-            <div>
+            <div className= "mb-35px">
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" >
                     <a className="navbar-brand" href="/">Carousel</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
