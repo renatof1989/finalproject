4f5673188255d6f72548c950468f1e2a97846a47
Naila Kaliyeva <nailakaliyeva@gmail.com>
2018-10-08 23:32:09 +0000

    done

--- src/js/component/total.jsx
+++ src/js/component/total.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import {Context} from '../store/appContext.jsx';
 function Total(){
-    return(
-        <div>
-            <div style = {{float: "right", opacity: "0.7"}}><br></br>$50.00</div>
-            <h5 style = {{}}><br></br>Total (USD)</h5>
-            
-        </div>
-        
-        );
-}
+        return(<Context.Consumer>
+                {
+                    ({store}) => {
+                        let total = 0;
+                        store.cart.forEach((item, index) => {total = total+item.price;});
+                     
+                        return(
+                            <div>
+                                <div style = {{float: "right", opacity: "0.7"}}><br></br>{'$' + (total-10)}</div>
+                                <h5 style = {{}}><br></br>Total (USD)</h5>
+                                
+                            </div>
+                           );
+                     }}
+                </Context.Consumer>
+            );
+    }
 export default Total;
