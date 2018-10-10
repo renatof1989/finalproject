0bb51ef28795ebfa290a039037c0dc7de8e0db02
Ignacio Cordoba <icordoba@4geeks.co>
2018-10-10 23:45:25 +0000

    Using fetch

--- src/js/store/appContext.jsx
+++ src/js/store/appContext.jsx
@@ -10,6 +10,27 @@ const Store = (PassedComponent) =>{
             this.state = getState(this);
         }
         
+        componentDidMount() {
+            
+            fetch('https://random-scripts-nachovz.c9users.io/collaborative-react-backend/products.json')
+              .then(response => response.json())
+              .then(data => {
+                  let {store} = this.state;
+                  store.products = data;
+                  this.setState({store});
+              })
+              .catch(error => console.log(error));
+              
+            fetch('https://random-scripts-nachovz.c9users.io/collaborative-react-backend/posts.json')
+            .then(response => {response.json();})
+            .then(iguana => { 
+              let {store} = this.state;
+              store.posts = iguana;
+              this.setState({store});
+            })
+            .catch(error => console.log(error));
+        }
+        
         render(){
             return(
                 <Context.Provider value={this.state}>
