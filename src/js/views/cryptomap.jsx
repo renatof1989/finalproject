ecf522ecea252a45726c57d015cf52577a766dfd
Javier Sarcar <libertypunk5@gmail.com>
2018-11-28 23:08:48 +0000

    moved crypto map and adjusted stuff

--- src/js/views/cryptomap.jsx
+++ src/js/views/cryptomap.jsx
@@ -5,13 +5,15 @@ import Bitcambio from '../component/bitcambio.jsx';
 import Braziliex from '../component/braziliex.jsx';
 import Negociecoins from '../component/negociecoins.jsx';
 import EuroExchange from '../component/euroexchange.jsx';
+import BasicMap from "../component/tooltip.jsx";
 import '../../styles/cryptomap.css';
 
 export class Cryptomap extends React.Component{
   render(){
     return(
-        <div className="container crypto">
-            <div className="row">
+        <div className="container">
+            <BasicMap />
+            <div className="row crypto">
                 <h1 className="col-12">Coin Compare Project</h1>
                 <div className="row">
                     <div className="col-4"><h1>United States</h1>
