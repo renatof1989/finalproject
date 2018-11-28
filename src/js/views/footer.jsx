03bee2666235272743b03ad902a38a8d58e3f798
Javier Sarcar <libertypunk5@gmail.com>
2018-11-28 23:58:36 +0000

    adjusted fonts

--- src/js/views/footer.jsx
+++ src/js/views/footer.jsx
@@ -1,4 +1,5 @@
  import React, {Component} from "react";
+ import '../../styles/footer.css';
  
  export default class Footer extends Component{
     render(){
@@ -6,8 +7,8 @@
             <footer className="container border-top p-5">
                 <div className="container ">
                     <div className="row">
-                        <div className="col-8">© 2017-2018 Company, Inc. · <a href="#ppage">Privacy</a> · <a href="#termspage">Terms</a></div>
-                        <div className="col-4"><a href="#top">Back to Top</a></div>
+                        <div className="col-8 font">© 2018 Company, Inc. · <a href="#ppage">Privacy</a> · <a href="#termspage">Terms</a></div>
+                        <div className="col-4 font"><a href="#top">Back to Top</a></div>
                     </div>
                 </div>
             </footer>
