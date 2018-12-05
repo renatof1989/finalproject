cf606d479344d368468bb2db347c170601f0995a
Javier Sarcar <libertypunk5@gmail.com>
2018-12-05 23:04:20 +0000

    pushing changes

--- src/js/views/footer.jsx
+++ src/js/views/footer.jsx
@@ -10,7 +10,7 @@
                         <div className="col-3 font">© 2018 Company, Inc. · <a href="#ppage">Privacy</a> · <a href="#termspage">Terms</a></div>
                         <div className="col-3 font"><a href="#top">Back to Top</a></div>
                         <div className="col-3"><a href="https://edge.app/bitcoin-wallet/">Get a free wallet HERE!</a></div>    
-                        <div className="col-3"><img style= {{width: 100, height: 50}} src="https://unhashed.com/wp-content/uploads/2018/03/Edge-Wallet-Review.png"/></div>
+                        <div className="col-3"><img alt="Edge Logo" style= {{width: 100, height: 50}} src="https://unhashed.com/wp-content/uploads/2018/03/Edge-Wallet-Review.png"/></div>
                     </div>
                 </div>
             </footer>
